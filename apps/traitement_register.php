1ab0d543b9caadce1dd0f40fd4ce7c625e369877
ZelusRL <zelus.guillaume@gmail.com>
2016-08-19 10:51:11 +0200

    blblblbl

--- apps/traitement_register.php
+++ apps/traitement_register.php
@@ -3,6 +3,7 @@ if(isset($_POST['username'], $_POST['mdp'], $_POST['mail']))
 {
 	$pseudo = $_POST['username'];
 	$password = $_POST['mdp'];
+	$password2 = $_POST['mdp2']; 
 	$mail = $_POST['mail'];
 	
 	if (strlen($pseudo) < 4 || strlen($pseudo) > 10)
@@ -13,6 +14,10 @@ if(isset($_POST['username'], $_POST['mdp'], $_POST['mail']))
 	else if (strlen($password) <4 || strlen($password) > 15) 
 	{
 		$error = "Le mot de passe doit comptenir entre 4 et 15 caractères.";
+	}
+	else if ($password != $password2)
+	{
+		$error = "Le mot de passe n'est pas confirmé";
 	}
 	else
 	{
